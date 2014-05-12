9e25fe70e023057f5a1d804838302154e951d194
Ted Morin <morinted@gmail.com>
2014-05-12 18:45:33 -0400

    Update version number in ClientGUI title bar

--- schedule-generator/ca/uottawa/ui/ClientGUI.java
+++ schedule-generator/ca/uottawa/ui/ClientGUI.java
@@ -549,7 +549,7 @@ public class ClientGUI implements ClientIF, ActionListener, DocumentListener, It
 	    {
 	    	port = DEFAULT_PORT; //Else default to the default port.
 	    } 
-		new ClientGUI("uOttawa Schedule Generator - v1.0.0", host, port);
+		new ClientGUI("uOttawa Schedule Generator - v1.0.1", host, port);
 	}
 
 	/**
