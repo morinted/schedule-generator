86cb9bf151d6a7af089abffb791aa67ed82d4530
David Schlachter <david+gh@schlachter.ca>
2019-04-29 21:01:14 -0400

    Show short activity location, fix #34

--- schedule-generator/ca/uottawa/ui/ClientGUI.java
+++ schedule-generator/ca/uottawa/ui/ClientGUI.java
@@ -984,6 +984,8 @@ public class ClientGUI implements ClientIF, ActionListener, DocumentListener, It
 				String actType = a.getType();
 				String strAct = actType + " " + a.getNumber();
 				String strLoc = a.getPlace();
+
+				strLoc = strLoc.replaceAll("^[^(]+\\(", "(").replaceAll("[()]", "");
 				
 				//We should find out what color this activity will be drawn in.
 				switch (actType) {
